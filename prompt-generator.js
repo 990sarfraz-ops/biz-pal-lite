ff123f6855a16dd3989486daa86deb6813d11c75
990sarfraz-ops <990sarfraz@gmail.com>
2026-01-13 08:40:47 -0800

    Add video form and output container
    <form id="videoForm">
      <input type="text" id="topic" placeholder="ویڈیو کا موضوع" required />
      <input type="number" id="duration" placeholder="دورانیہ (منٹ)" required />
      <button type="submit">Generate Video Script</button>
    </form>
    
    <div id="videoOutput"></div>
    
    <script src="video-generator.js"></script>
    <script src="index.js"></script>

---
+++ prompt-generator.js
@@ -0,0 +1,10 @@
+<form id="videoForm">
+  <input type="text" id="topic" placeholder="ویڈیو کا موضوع" required />
+  <input type="number" id="duration" placeholder="دورانیہ (منٹ)" required />
+  <button type="submit">Generate Video Script</button>
+</form>
+
+<div id="videoOutput"></div>
+
+<script src="video-generator.js"></script>
+<script src="index.js"></script>
